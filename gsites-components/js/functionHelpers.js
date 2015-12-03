52985f73cc57482393a2664e1a354123a50607be
David Peris Martinez <david.peris92@gmail.com>
2015-12-03 11:48:03 +0100

    añadido cambio a functions

--- gsites-components/js/functionHelpers.js
+++ gsites-components/js/functionHelpers.js
@@ -1,8 +1,15 @@
 
 
 
-function adjustGadgetHeight(div) {
+function adjustGadgetHeight() {
 
-	gadgets.window.adjustHeight(Math.ceil(document.getElementById(div).offsetHeight));
+	var D = document;
+    var height = Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+
+	gadgets.window.adjustHeight(Math.ceil(height));
 
 }
