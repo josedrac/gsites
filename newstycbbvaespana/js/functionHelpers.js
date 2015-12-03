db1f0a19f6d508ff504e2dd2e9e30e327496feea
David Peris Martinez <david.peris92@gmail.com>
2015-12-03 14:05:31 +0100

    modificada función de autoajuste de la altura

--- newstycbbvaespana/js/functionHelpers.js
+++ newstycbbvaespana/js/functionHelpers.js
@@ -1,15 +1,35 @@
 
 
 
-function adjustGadgetHeight() {
+function adjustGadgetHeight(div) {
+	onElementHeightChange(document.getElementById(div),
+	 	function(){
+			var D = document;
+		    var height = Math.max(
+		    	document.getElementById(div).offsetHeight,
+		        D.body.scrollHeight, D.documentElement.scrollHeight,
+		        D.body.offsetHeight, D.documentElement.offsetHeight,
+		        D.body.clientHeight, D.documentElement.clientHeight
+			);
 
-	var D = document;
-    var height = Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
-        D.body.clientHeight, D.documentElement.clientHeight
-    );
-
-	gadgets.window.adjustHeight(Math.ceil(height));
-
+			gadgets.window.adjustHeight(Math.ceil(height));
+		});
 }
+
+function onElementHeightChange(elm, callback){
+    var lastHeight = elm.clientHeight, newHeight;
+    (function run(){
+        newHeight = elm.clientHeight;
+        if( lastHeight != newHeight)
+            callback();
+
+        lastHeight = newHeight;
+
+        if( elm.onElementHeightChangeTimer )
+            clearTimeout(elm.onElementHeightChangeTimer);
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 200);
+    })();
+}
+
+				
