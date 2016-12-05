7093d805a69debee26abdd1269ffbb5f30c9b044
enriquegilarcas <enriquegilarcas.sopra@gmail.com>
2016-12-05 10:08:08 +0100

    1.0

--- evolucionaDigital/js/environment.js
+++ evolucionaDigital/js/environment.js
@@ -6,7 +6,7 @@ window.Environment = function(){
     var params = "select * ";
 
     function sendQuery(url, params, callback){
-        console.log('Getting token');
+        //console.log('Getting token');
         var opts = {dataType:'jsonp'};
         var query = new google.visualization.Query(url, opts);
         query.setQuery(params);
@@ -15,15 +15,15 @@ window.Environment = function(){
 
     // It Receive an url to be autorized and a callback to inject it at the end;
     var sendAuthorizedQuery = function(url, request) {
-        console.log("Autorizing: ", url);
+        //console.log("Autorizing: ", url);
 
         var callback = function (response) {
             if (response && !response.error){
                 var data = response.getDataTable();
                 var token = data.getValue(0,0);
-                console.log('Token fetched: ', token);
+                //console.log('Token fetched: ', token);
                 var authorized_url = url + "?access_token=" + encodeURIComponent(token);
-                console.log('authorized_url: ', authorized_url);
+                //console.log('authorized_url: ', authorized_url);
                 request(authorized_url);
             } else {
                 console.error("Error fetching the token: " + response.error);
